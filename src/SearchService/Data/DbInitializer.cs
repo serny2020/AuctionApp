bf45ef5860a721a790c492c44f4451f6f87a3421
Shawn <59898014+serny2020@users.noreply.github.com>
2025-03-03 02:54:55 +0000

    HTTP request between AuctionService and SearchService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
@@ -9,7 +10,7 @@ namespace SearchService.Data
     {
         public static async Task InitDB(WebApplication app)
         {
-            await DB.InitAsync("SearchDB", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDBConnection"))); 
+            await DB.InitAsync("SearchDB", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDBConnection")));
 
             await DB.Index<Item>()
                 .Key(a => a.Make, KeyType.Text)
@@ -19,13 +20,25 @@ namespace SearchService.Data
 
             var count = await DB.CountAsync<Item>();
 
-            if (count == 0) {
-                System.Console.WriteLine("No data found. Seeding data...");
-                var itemData = await System.IO.File.ReadAllTextAsync("Data/auctions.json");
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            // if (count == 0) {
+            //     System.Console.WriteLine("No data found. Seeding data...");
+            //     var itemData = await System.IO.File.ReadAllTextAsync("Data/auctions.json");
+            //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            //     await DB.SaveAsync(items);
+            // }
+            using var scope = app.Services.CreateScope();
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.WriteLine(items.Count + " returned from the auction service");
+
+            if (items.Count > 0)
+            {
                 await DB.SaveAsync(items);
             }
+
         }
     }
 }
