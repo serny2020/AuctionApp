0d4332916ddc0743646390ba9e2cac5d1cce6199
Shawn <59898014+serny2020@users.noreply.github.com>
2025-03-03 01:21:49 +0000

    pagination

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,23 +11,26 @@ public class SearchController : ControllerBase
 
     [HttpGet]
     /// <summary>
-    /// Searches for items based on the provided search term.
+    /// Searches for items based on the provided search term with pagination.
     /// </summary>
-    /// <param name="searchItem">The search term to look for.</param>
+    /// <param name="searchTerm">The search term to look for.</param>
+    /// <param name="pageNumber">The page number to retrieve.</param>
+    /// <param name="pageSize">The number of items per page.</param>
     /// <returns>A list of items that match the search term.</returns>
-    public async Task<IActionResult> SearchItems(string searchTerm)
+    public async Task<IActionResult> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
     {
-        var query = DB.Find<Item>()
+        var query = DB.PagedSearch<Item>()
                       .Sort(x => x.Ascending(a => a.Make));
 
         if (!string.IsNullOrEmpty(searchTerm))
         {
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
-
+        query.PageSize(pageSize);
+        query.PageNumber(pageNumber);
         var items = await query.ExecuteAsync();
 
-        return Ok(items);
+        return Ok(new { results = items.Results, pageCount=items.PageCount, totalCount=items.TotalCount });
     }
     
 }      
