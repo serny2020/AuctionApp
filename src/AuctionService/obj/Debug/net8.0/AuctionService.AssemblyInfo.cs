98d58fe4765985b1838af7fc9327913992f250ae
Shawn <59898014+serny2020@users.noreply.github.com>
2025-03-05 03:14:44 +0000

    added sample authenticated users

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0602b715caa54a25e9c12535eb94325558a3f72d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7be8371d78613c21716ed509ad7ba6728c37d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
