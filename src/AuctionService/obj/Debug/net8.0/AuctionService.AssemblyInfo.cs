a14530e52a556abe578d2c947609c73548373038
Shawn <59898014+serny2020@users.noreply.github.com>
2025-03-08 20:43:11 +0000

    additional local addresses for identity svc

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaecac0b54710c521c8ff0924d4483835642e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6126c1aa15dd6d788e2be89029d755cb1a0ced81")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
