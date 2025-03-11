d49eef3c53667a65ab5d5f0b52dbc09088677e5d
Shawn <59898014+serny2020@users.noreply.github.com>
2025-03-11 02:36:19 +0000

    Pending changes exported from your codespace

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6126c1aa15dd6d788e2be89029d755cb1a0ced81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14530e52a556abe578d2c947609c73548373038")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
